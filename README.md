ca4fff15696920604635f30354d03cfc68090099
Tu Huynh <tuhuynh.dev@gmail.com>
2024-09-28 12:19:07 +0700

    feat: encrypt storage

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Please create and environment variable named `NEXT_PUBLIC_OPEN_WEATHER_API_KEY` with your open weather api key.
+Please create and environment variable named:
+
+- `NEXT_PUBLIC_OPEN_WEATHER_API_KEY` with your open weather api key.
+- `NEXT_PUBLIC_ENCRYPTION_KEY` with your encryption key.
--- src/store/location.ts
+++ src/store/location.ts
@@ -1,6 +1,34 @@
 import { LocationData } from "@/types/api";
 import { create } from "zustand";
-import { createJSONStorage, persist } from "zustand/middleware";
+import { createJSONStorage, persist, StateStorage } from "zustand/middleware";
+import CryptoJS from "crypto-js";
+
+const encryptedStorage: StateStorage = {
+  getItem(key: string): any | undefined {
+    const value = localStorage.getItem(key);
+
+    if (value) {
+      const decryptedBytes = CryptoJS.AES.decrypt(
+        value,
+        process.env.NEXT_PUBLIC_ENCRYPTION_KEY!
+      );
+      const decryptedValue = decryptedBytes.toString(CryptoJS.enc.Utf8);
+      return decryptedValue;
+    }
+
+    return value;
+  },
+  setItem(key: string, value: any): void {
+    const encrypted = CryptoJS.AES.encrypt(
+      value,
+      process.env.NEXT_PUBLIC_ENCRYPTION_KEY!
+    ).toString();
+    localStorage.setItem(key, encrypted);
+  },
+  removeItem(key: string): void {
+    localStorage.removeItem(key);
+  },
+};
 
 interface LocationStore {
   currentLocation: LocationData;
@@ -11,10 +39,10 @@ interface LocationStore {
 }
 
 const defaultLocation: LocationData = {
-  name: "Singapore, SG",
+  name: "Singapore",
   lat: 1.3521,
   lon: 103.8198,
-  country: "Singapore",
+  country: "SG",
   state: "Singapore",
 };
 
@@ -27,12 +55,15 @@ export const useLocationStore = create<LocationStore>()(
       addToHistory: (location) =>
         set({ history: [location, ...get().history] }),
       removeFromHistory: (location) =>
-        set({ history: get().history.filter((item) => item !== location) }),
-      clearHistory: () => set({ history: [] }),
+        set({
+          history: get().history.filter(
+            (item) => item.lat !== location.lat && item.lon !== location.lon
+          ),
+        }),
     }),
     {
       name: "location-store",
-      storage: createJSONStorage(() => localStorage),
+      storage: createJSONStorage(() => encryptedStorage),
     }
   )
 );
